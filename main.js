4baabd5d98fd09615005cc72258f939c58ec903a
Johannes Vihavainen <johannesvihavainen@yahoo.com>
2025-01-04 18:29:43 -0600

    updated index.html tags, main.css styling for search functionality, and main.js to make input search for cars function

--- main.js
+++ main.js
@@ -1,20 +1,73 @@
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+var input = document.querySelector('.search-car');
+var nissan = document.querySelector('.skyline');
+var challenger = document.querySelector('.challenger');
+var toyota = document.querySelector('.toyota');
+var rover = document.querySelector('.rover');
+var gridContainer = document.querySelector('.grid-container');
+
+var addToCart = document.getElementsByClassName('add-to-cart');
+
+for(i = 0; i < addToCart.length; i++) {
+    addToCart[i].addEventListener("click", function () {
+        console.log("btn was clicked")
+        updateCart();
+    })
 }
 
-window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+
+
+var cartCountTag = document.querySelector('.cart-counter');
+var cartCounter = 0;
+function updateCart() {
+    cartCounter++;
+    cartCountTag.innerHTML = `(${cartCounter})`;
+}
+
+input.addEventListener("input", function (event) {
+    const inputValue = event.target.value.toLowerCase();
+    if (inputValue === "nissan" || inputValue === "skyline") {
+        nissan.style.display = "block";
+        challenger.style.display = "none";
+        toyota.style.display = "none";
+        rover.style.display = "none";
+        gridContainer.style.display = "flex";
     }
-}
-
+    else if (inputValue === "challenger") {
+        nissan.style.display = "none";
+        challenger.style.display = "block";
+        toyota.style.display = "none";
+        rover.style.display = "none";
+        gridContainer.style.display = "flex";
+    }
+    else if (inputValue === "toyota") {
+        nissan.style.display = "none";
+        challenger.style.display = "none";
+        toyota.style.display = "block";
+        rover.style.display = "none";
+        gridContainer.style.display = "flex";
+    }
+    else if (inputValue === "rover") {
+        nissan.style.display = "none";
+        challenger.style.display = "none";
+        toyota.style.display = "none";
+        rover.style.display = "block";
+        gridContainer.style.display = "flex";
+    }
+    else if (inputValue === "") {
+        nissan.style.display = "block";
+        challenger.style.display = "block";
+        toyota.style.display = "block";
+        rover.style.display = "block";
+        gridContainer.style.display = "grid";
+    }
+    else {
+        nissan.style.display = "block";
+        challenger.style.display = "block";
+        toyota.style.display = "block";
+        rover.style.display = "block";
+        gridContainer.style.display = "grid";
+    }
+})
 
 let slideIndex = 1;
 showSlides(slideIndex);
